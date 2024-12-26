2ffe808610ea1dd45ca325d924b4db1cfa86b3ed
= <=>
2024-12-26 11:32:56 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from 'react';
+import Form from './Components/Form';
+export const MyContext = createContext();
+
+function App() {
+  const [prod, setProd] = useState([])
+  return (
+    <div className="App">
+      <MyContext.Provider value={{ prod, setProd }}>
+        <Form />
+      </MyContext.Provider>
+    </div>
+  );
+}
+
+export default App;
+
